681828d49dbfdc3b860255b0b7cdb78f087b7cdb
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-04-21 21:43:22 +0100

    create autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+//Autobind decorator
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor): any {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -30,13 +44,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @autoBind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
