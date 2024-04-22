61a415a4b207cd26cde4db93beb99b9738d2cf94
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-04-22 18:57:56 +0100

    create the droppable Area

--- src/app.ts
+++ src/app.ts
@@ -184,7 +184,8 @@ class ProjectItem
 
   @autoBind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
   dragEndHandler(_: DragEvent) {
     console.log("Drag End");
@@ -220,12 +221,17 @@ class ProjectList
   }
 
   @autoBind
-  dragOverHandler(_: DragEvent) {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
 
-  dropHandler(_: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData("text/plain"));
+  }
 
   @autoBind
   dragLeaveHandler(_: DragEvent) {
